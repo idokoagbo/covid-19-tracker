68033e9bd05a553e1be646e43f2d5d9059284f91
Idoko Agbo <idokojudge@gmail.com>
2022-03-05 14:02:31 -0500

    hotfix: receive case reports as number

--- lib/model/totals.dart
+++ lib/model/totals.dart
@@ -1,26 +1,25 @@
-class Totals{
+class Totals {
   int confirmed;
   int recovered;
   int critical;
   int deaths;
 
-  Totals(this.confirmed,this.recovered,this.critical,this.deaths);
+  Totals(this.confirmed, this.recovered, this.critical, this.deaths);
 
-  Totals.map(dynamic obj){
-    this.confirmed=int.parse(obj['confirmed']);
-    this.recovered=int.parse(obj['recovered']);
-    this.critical=int.parse(obj['critical']);
-    this.deaths=int.parse(obj['deaths']);
+  Totals.map(dynamic obj) {
+    this.confirmed = obj['cases'];
+    this.recovered = obj['recovered'];
+    this.critical = obj['critical'];
+    this.deaths = obj['deaths'];
   }
 
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
 
-  Map<String, dynamic> toMap(){
-    var map=new Map<String, dynamic>();
-
-    map['confirmed']="$confirmed";
-    map['recovered']="$recovered";
-    map['critical']="$critical";
-    map['deaths']="$deaths";
+    map['cases'] = confirmed;
+    map['recovered'] = recovered;
+    map['critical'] = critical;
+    map['deaths'] = deaths;
 
     return map;
   }
