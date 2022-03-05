269c204dd99eb4d43f747583c29adbda8760808d
Idoko Agbo <idokojudge@gmail.com>
2022-03-05 14:02:10 -0500

    hotfix: country model to disease sh data structure

--- lib/model/country.dart
+++ lib/model/country.dart
@@ -1,35 +1,35 @@
-class Country{
+class Country {
   String name;
   int confirmed;
   int recovered;
   int critical;
   int deaths;
-  double latitude;
-  double longitude;
+  int latitude;
+  int longitude;
 
-  Country(this.name,this.confirmed,this.recovered,this.critical,this.deaths,this.latitude,this.longitude);
+  Country(this.name, this.confirmed, this.recovered, this.critical, this.deaths,
+      this.latitude, this.longitude);
 
-  Country.map(dynamic obj){
-    this.name=obj['country'];
-    this.confirmed=obj['confirmed'];
-    this.recovered=obj['recovered'];
-    this.critical=obj['critical'];
-    this.deaths=obj['deaths'];
-    this.latitude=obj['latitude'];
-    this.longitude=obj['longitude'];
+  Country.map(dynamic obj) {
+    this.name = obj['country'];
+    this.confirmed = obj['cases'];
+    this.recovered = obj['recovered'];
+    this.critical = obj['critical'];
+    this.deaths = obj['deaths'];
+    this.latitude = obj['countryInfo']['lat'];
+    this.longitude = obj['countryInfo']['long'];
   }
 
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
 
-  Map<String, dynamic> toMap(){
-    var map=new Map<String, dynamic>();
-
-    map['name']=name;
-    map['confirmed']="$confirmed";
-    map['recovered']="$recovered";
-    map['critical']="$critical";
-    map['deaths']="$deaths";
-    map['latitude']="$latitude";
-    map['longitude']="$longitude";
+    map['name'] = name;
+    map['cases'] = confirmed;
+    map['recovered'] = recovered;
+    map['critical'] = critical;
+    map['deaths'] = deaths;
+    map['latitude'] = latitude;
+    map['longitude'] = longitude;
 
     return map;
   }
